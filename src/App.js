809833624ea9a2dc533b34c0922f39c925507e18
Aday Mesa <aday.mesa-ojeda_ext@scout24.com>
2017-09-18 00:36:23 +0200

    manage react component state and lifecycle methods

--- src/App.js
+++ src/App.js
@@ -12,16 +12,20 @@ class App extends React.Component {
 	}
 	componentWillMount(){
 		console.log('componentWillMount')
+		this.setState({m: 2})
 	}
 	render(){
 		console.log('render')
-		return <button onClick={this.update}>{this.state.val}</button>
+		return <button onClick={this.update}>{this.state.val * this.state.m}</button>
+
 	}
 	componentDidMount(){
 		console.log('componentDidMount')
+		this.inc = setInterval(this.update, 500)
 	}
 	componentWillUnnount(){
 		console.log('componentWillUnnount')
+		clearInterval(this.inc)
 	}
 
 }

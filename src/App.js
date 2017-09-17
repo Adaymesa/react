ecd6abdb3b96a12f5060705adde321fc2c17b6ae
Aday Mesa <aday.mesa-ojeda_ext@scout24.com>
2017-09-17 23:56:05 +0200

    access nested data with react children

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import React from "react";
 
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			a: ''
-		}
-	}
-
-	update(e){
-		this.setState({
-			a: this.a.refs.input.value,
-			b: this.refs.b.value
-	})
-	}
 	render(){
-		return (
-			<div>
-				<Input ref={component => this.a = component} update={this.update.bind(this)}/>
-				{this.state.a}
-				<hr />
-				<input ref="b" onChange={this.update.bind(this)}/>
-				{this.state.b}
-			</div>
-			) 
+		return <Button> I <Heart /> React </Button>
 	}
 }
 
-class Input extends React.Component {
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component {
 	render() {
-		return <input ref="input" type="text" onChange={this.props.update}/>
+		return <span>&hearts;</span>
 	}
 }
 
